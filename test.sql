3df556d7a592b1ddbb12f54a6bc7f7ccf1dfa0b9
manishaschavan <87010609+manishaschavan@users.noreply.github.com>
2022-01-06 23:11:32 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE studentData(Name varchar(20),Roll_no integer,Address varchar(40),Mobile_Number biginit primary key NOT NULL,PAN_Number varchar(10));
+INSERT INTO studentData values('manisha',20112046,'Jalgoan',9307818588,'PAN12345');
+INSERT INTO studentData values('anku',20112011,'Chandigarh',82849855738,'PAN12345');
+INSERT INTO studentData values('rucha',20112042,'pune',9207818558,'PAN12345');
+INSERT INTO studentData values('mayuri',20112048,'pune',98756567788,'PAN12345');
+INSERT INTO studentData values('hrutu',20112044,'pune',9090988776,'PAN12345');
+
+
